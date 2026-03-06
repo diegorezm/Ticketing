2ce65dd2256635980519d9a01fad3f944ef3ef08
diegorezm <dmourarezende@gmail.com>
2026-03-06 11:03:11 -0300

    simpler function

--- src/features/auth/actions/get-session-action.ts
+++ src/features/auth/actions/get-session-action.ts
@@ -2,12 +2,12 @@ import { createServerFn } from '@tanstack/react-start'
 import { getRequestHeaders } from '@tanstack/react-start/server'
 import { auth } from '../lib/auth'
 
-export const getSessionAction = createServerFn().handler(async () => {
-  const headers = getRequestHeaders()
+export const getSessionAction = createServerFn({ method: 'GET' }).handler(
+  async () => {
+    const headers = getRequestHeaders()
 
-  const session = await auth.api.getSession({
-    headers,
-  })
-
-  return session
-})
+    return await auth.api.getSession({
+      headers,
+    })
+  },
+)
